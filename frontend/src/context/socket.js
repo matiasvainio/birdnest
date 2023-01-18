fcefa2ac03d2e4546b7a1b1baec98cc2cc9f7783
Matias Vainio <matias.s.vainio@gmail.com>
2023-01-18 22:43:27 +0200

    new url

--- frontend/src/context/socket.js
+++ frontend/src/context/socket.js
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
-export const socket = io.connect("long-grass-5744.fly.dev");
+export const socket = io.connect("cool-snow-4493.fly.dev");
 // export const socket = io.connect("localhost:3000");
 export const SocketContext = createContext();
