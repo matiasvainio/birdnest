d0d2bb8fc06e7def3c92b218d2c6faafe56c1cd0
Matias Vainio <matias.s.vainio@gmail.com>
2023-01-18 18:27:37 +0200

    url for production added

--- frontend/src/context/socket.js
+++ frontend/src/context/socket.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
-export const socket = io.connect("localhost:3000");
+export const socket = io.connect("long-grass-5744.fly.dev");
 export const SocketContext = createContext();
